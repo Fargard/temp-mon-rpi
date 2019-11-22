0aa02fd19a149489415974bdaacf46a2d79caf82
Nikolay Seleznev <nnseleznev@gmail.com>
2019-11-22 10:49:59 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: ['@babel/preset-react']
+          }
+        }]
+      }
+    ]
+  }
+};
